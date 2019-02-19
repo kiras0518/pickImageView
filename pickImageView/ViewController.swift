87fce48f1caf2794d38e109738ff5f893d2b2993
雨夜 <kiras0518@gmail.com>
2019-02-19 15:11:46 +0800

    get images from the  photo library

--- pickImageView/ViewController.swift
+++ pickImageView/ViewController.swift
@@ -8,15 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    let image = UIImagePickerController()
+    
+    @IBAction func pickView(_ sender: UIButton) {
+       
+        image.delegate = self
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        
+        self.present(image, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.originalImage] as? UIImage {
+            myImageView.image = image
+        } else {
+        
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("hello")
+         //myImageView.image = image
     }
-
-
 
 }
 
